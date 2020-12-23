04f4cea0d183928ef70fbab09c83c608eaacda50
jay <jason.primeau@gmail.com>
2020-12-23 16:36:02 -0500

    v1.0

--- swearbot.js
+++ swearbot.js
@@ -1,25 +1,23 @@
 const retorts = require("./retorts");
 
 // see also: https://www.youtube.com/watch?v=kyBH5oNQOS0
-const dirtyWords = RegExp(
-  `shit|piss|fuck|cunt|cocksucker|motherfucker|tits|dick|asshole|bitch`
-);
-
+const dirtyWords = RegExp(`shit|piss|fuck|cunt|cocksucker|motherfucker|tits|dick|asshole|bitch`);
 const MAX_FUCKS_ALLOWED = 3;
-const FUCK_WINDOW = 300;
+const FUCK_WINDOW = 300; // time range in seconds
 const WITTY_RETORTS = [
   `You're quite the pottymouth, aren't you {NAME}?`,
   `My EARS {NAME}!`,
   `Oh My Goodness {NAME}, that's quite enough cursing!`,
 ];
 
-let fuckCounter = {};
+// stores arrays of timestamps for everybody's swears
+let swearJar = {};
 
 exports.respondToMessage = (message) => {
   let response;
   if (dirtyWords.test(message.text)) {
     // load up the swears
-    let fucksGiven = fuckCounter[message.user] || [];
+    let fucksGiven = swearJar[message.user] || [];
     // add to the swear jar
     fucksGiven.push(message.ts);
     // we only want the most recent swears
@@ -32,10 +30,11 @@ exports.respondToMessage = (message) => {
       let oldestFuck = fucksGiven[0];
       // have they been swearing a lot lately?
       if (newestFuck - oldestFuck < FUCK_WINDOW) {
+        // dirty mouth detected, prepare a response
         response = retorts.select(WITTY_RETORTS, message.user);
       }
     }
-    fuckCounter[message.user] = fucksGiven;
+    swearJar[message.user] = fucksGiven;
   }
   return response;
 };
--- threadbot.js
+++ threadbot.js
@@ -1,37 +1,30 @@
 const retorts = require("./retorts");
 
 const THREAD_LENGTH_WARN = process.env.THREAD_LENGTH_WARN || 50;
-
-let threadCounter = {};
-
 const WITTY_RETORTS = [
   `Gosh, this sure is getting to be quite the thread isn't it {NAME}?`,
   `Hmmm, maybe it's time we took this to a room, don't you think {NAME}?`,
   `Ok, no really, let's take it to Zoom you chatterboxes. Especially you, {NAME}!`,
   `I think this channel has had enough of your jibber-jabber {NAME}!`,
 ];
+let threadCounter = {};
 
-
+//
+// read a message
+// figure out if it's in a thread
+// return a message if the thread is getting too long, or nothing
+//
 exports.respondToMessage = (message) => {
-
-  // THREAD_LENGTH_WARN = process.env.THREAD_LENGTH_WARN || 50;
-
-  console.log(THREAD_LENGTH_WARN);
-
-
   let response;
-  let zoomIt = false;
+  // check if the message is in a thread 
   if (message.thread_ts) {
+    // get the thread count so far, or initialize to 0
     let count = threadCounter[message.thread_ts] || 0;
     threadCounter[message.thread_ts] = ++count;
-
+    // if this message is a multiple of the threshold, prepare a response
     if (count && count % THREAD_LENGTH_WARN === 0) {
       response = retorts.select(WITTY_RETORTS, message.user);
     }
   }
-
-  //   if (zoomIt) {
-  //   await say(`/zoom`); //TODO
-  // }
   return response;
 };
