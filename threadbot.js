f26ed002a5fc7f4c11b8495e76afbe9233d62080
jay <jason.primeau@gmail.com>
2020-12-23 14:25:26 -0500

    witty retorts

--- threadbot.js
+++ threadbot.js
@@ -1,7 +1,12 @@
-const THREAD_LENGTH_WARN_1 = 50;
-const THREAD_LENGTH_WARN_2 = 100;
-const THREAD_LENGTH_WARN_3 = 150;
+const THREAD_LENGTH_WARN = 2;
 let threadCounter = {};
+
+const WITTY_RETORTS = [
+  `Gosh, this sure is getting to be quite the thread isn't it <@${message.user}>?`,
+  `Hmmm, maybe it's time we took this to a room, right <@${message.user}>?`,
+  `Ok, no really, here's a Zoom link for you chatterboxes. Especially you, <@${message.user}>!`,
+];
+
 
 exports.readMessage = (message) => {
   let response;
@@ -10,17 +15,18 @@ exports.readMessage = (message) => {
     let count = threadCounter[message.thread_ts] || 0;
     threadCounter[message.thread_ts] = ++count;
 
-    if (count === THREAD_LENGTH_WARN_1) {
-      response = `Gosh, this sure is getting to be quite the thread isn't it <@${message.user}>?`;
-    } else if (count === THREAD_LENGTH_WARN_2) {
-      response = `Hmmm, maybe it's time we took this to a room, right <@${message.user}>?`;
-    } else if (count === THREAD_LENGTH_WARN_3) {
-      response = `Ok, no really, here's a Zoom link for you chatterboxes. Especially you, <@${message.user}>!`;
-      zoomIt = true;
+    if (count && count % THREAD_LENGTH_WARN) {
+      response = pickWittyRetort();
     }
   } 
+
   //   if (zoomIt) {
   //   await say(`/zoom`); //TODO
   // }
   return response;
 };
+
+
+function pickWittyRetort() {
+  return WITTY_RETORTS[Math.floor(Math.random() * WITTY_RETORTS.length)];
+}
