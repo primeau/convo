838ad99c0d9d315a56f99c463e96dc8c63430f06
jay <jason.primeau@gmail.com>
2020-12-23 16:08:29 -0500

    thread length env var

--- threadbot.js
+++ threadbot.js
@@ -1,6 +1,7 @@
 const retorts = require("./retorts");
 
-const THREAD_LENGTH_WARN = process.env.THREAD_LENGTH_WARN || 50;
+let THREAD_LENGTH_WARN;
+
 let threadCounter = {};
 
 const WITTY_RETORTS = [
@@ -12,6 +13,12 @@ const WITTY_RETORTS = [
 
 
 exports.respondToMessage = (message) => {
+
+  THREAD_LENGTH_WARN = process.env.THREAD_LENGTH_WARN || 50;
+
+  console.log(THREAD_LENGTH_WARN);
+
+
   let response;
   let zoomIt = false;
   if (message.thread_ts) {
